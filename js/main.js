4d509332c8940f0d1a0e3dcde995e76e66e33033
leonardoserra <serra.leonardo92@gmail.com>
2023-02-23 16:59:35 +0100

    provo a fare uscire il prezzo..

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ const generator = document.querySelector("#generator");
 const reset = document.querySelector("#reset");
 const ageRange = document.querySelector("#ageRange");
 const priceTab = document.querySelector("#priceTab");
-const pricePerKm = 0.21;
 const passengerCode = document.getElementById('passengerCode');
+const errorMessage = document.getElementById('errorMessage');
+const pricePerKm = 0.21;
+const nameSurname = document.querySelector("#nameSurname");
+const tarifs = document.querySelector("#tarifs");
+
 
 let km = document.querySelector("#km");
-let nameSurname = document.querySelector("#nameSurname");
-let tarifs = document.querySelector("#tarifs");
-let price = pricePerKm * km;
+
 let randomNumber;
 //console.log(typeof(parseInt(km)));
 
@@ -18,14 +20,36 @@ let randomNumber;
 
 //confirm button
 generator.addEventListener('click', function() {
-   
+    price = pricePerKm * km;
+    console.log(pricePerKm * km);
     nameSurname.innerHTML = username.value;
     tarifs.innerHTML = ageRange.value;
-    priceTab.innerHTML = price;
+    
     randomNumber = Math.floor(Math.random() * 10000 ) + 90000;
     passengerCode.innerHTML = randomNumber;
 
+    // if(isNaN(parseInt(km))){
+    //     errorMessage.innerHTML = 'ATTENZIONE: Inserire solo numeri nel campo "Km da percorrere"'
+    // } else {
+        
+        if(ageRange.value == "standard"){
+            priceTab.innerHTML = price;
+            console.log(price);
 
+        } else if(ageRange.value == "under18"){
+            priceTab.innerHTML = price - (price * 100 / 20);
+            console.log(price);
+            
+        } else if(ageRange.value == "over65"){
+            priceTab.innerHTML = price - (price * 100 / 40);
+            console.log(price);
+        } else {
+            errorMessage.innerHTML = 'ATTENZIONE: Selezionare tariffa prima di premere "Genera Biglietto"'
+        }
+    // }
+
+
+    console.log(price);
     console.log(randomNumber);
     console.log(`${username.value}` + ` ${parseInt(km.value)}` + ` ${ageRange.value}`);
     console.log(typeof(parseInt(km)));
