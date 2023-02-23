62f3cc02a71ac83f679e7a90886b2e93b5c58307
leonardoserra <serra.leonardo92@gmail.com>
2023-02-23 15:53:28 +0100

    estratto number da #km con parse int

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ const generator = document.querySelector("#generator");
 const reset = document.querySelector("#reset");
 const ageRange = document.querySelector("#ageRange");
 //km = parseInt(km);
-console.log(ageRange.value);
-
 
 generator.addEventListener('click', function() {
     //if(isNaN(km)) {
    //     console.log("ATTENZIONE: Inserire solo numeri dentro casella Km da percorrere");
     //}else{
-        console.log(`${username.value}` + ` ${km.value}` + ` ${ageRange.value}`);
+        console.log(`${username.value}` + ` ${parseInt(km.value)}` + ` ${ageRange.value}`);
+        console.log(typeof(parseInt(km)));
 
     //}
     
