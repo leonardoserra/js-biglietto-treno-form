b9b95e914c38c20ced44322ba092d3f924a490bc
leonardoserra <serra.leonardo92@gmail.com>
2023-02-23 16:18:31 +0100

    prove...

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 const username = document.querySelector("#username");
-const km = document.querySelector("#km");
+let km = document.querySelector("#km");
 const generator = document.querySelector("#generator");
 const reset = document.querySelector("#reset");
 const ageRange = document.querySelector("#ageRange");
-//km = parseInt(km);
+const priceTab = document.querySelector("#priceTab");
+const pricePerKm = 0.21;
 
+let nameSurname = document.querySelector("#nameSurname");
+let tarifs = document.querySelector("#tarifs");
+let price = pricePerKm * parseInt(km);
+console.log(typeof(parseInt(km)));
+//km = parseInt(km.value);
+
+
+
+//confirm button
 generator.addEventListener('click', function() {
+
     //if(isNaN(km)) {
-   //     console.log("ATTENZIONE: Inserire solo numeri dentro casella Km da percorrere");
+        //console.log("ATTENZIONE: Inserire solo numeri dentro casella Km da percorrere");
     //}else{
         console.log(`${username.value}` + ` ${parseInt(km.value)}` + ` ${ageRange.value}`);
         console.log(typeof(parseInt(km)));
+
+        nameSurname.innerHTML = username.value;
+        tarifs.innerHTML = ageRange.value;
+        priceTab.innerHTML = parseInt(price);
+        
 
     //}
     
 });
 
+
+//reset button
 reset.addEventListener('click', function() {
 
     username.value = '';
